409cf0ecde8375f3645c2f8d6b0f3fa5bb79c9ee
hobofan <hobofan@users.noreply.github.com>
2020-12-29 19:10:17 +0000

    deploy: 9752c7f73c109bef321e17d2cdba3f332153d7b7

---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["extendr_api"] = {"name":"","files":["args.rs","error.rs","lib.rs","logical.rs","rmacros.rs","robj.rs","wrapper.rs"]};
+sourcesIndex["extendr_engine"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["extendr_macros"] = {"name":"","files":["lib.rs","output_r.rs"]};
+createSourceSidebar();
